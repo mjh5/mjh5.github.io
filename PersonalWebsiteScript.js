decf5fa49c55201489feb630a63d4ec07f3dd9ec
mjh5 <mjh5@illinois.edu>
2018-06-04 13:29:40 -0500

    Wrote findTenDigitNumber, isPrime needs to be written

--- PersonalWebsiteScript.js
+++ PersonalWebsiteScript.js
@@ -1,3 +1,23 @@
 function findTenDigitPrime(numberToSearch) {
-  
+  var numberAsString = processNumber(numberToSearch);
+  for (var i = 0; i < numberAsString.length - 11; i++) {
+    let tenDigits = numberAsString.substring(i, i + 11);
+    let tenDigitNumber = parseInt(tenDigits);
+    if (isPrime(tenDigitNumber) {
+      return tenDigitNumber;
+    }
+  }
+
+  return "No ten digit prime number was found";
+}
+
+function processNumber(num) {
+  var numberAsString = num.toString();
+  var processedNum = num.replace('.', "");
+
+  return processedNum;
+}
+
+function isPrime(num) {
+
 }
