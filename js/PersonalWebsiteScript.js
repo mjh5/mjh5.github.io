75e5b9b09ffd306fe82c5b310b36a4fa05386f46
mjh5 <mjh5@illinois.edu>
2018-11-17 12:30:56 -0600

    Wrote isPrime helper method, need to test

--- js/PersonalWebsiteScript.js
+++ js/PersonalWebsiteScript.js
@@ -18,8 +18,27 @@ function processNumber(num) {
   return processedNum;
 }
 
+//Implementation of the Primality Test
+//https://en.wikipedia.org/wiki/Primality_test#Pseudocode
 function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  } else if (num <= 3) {
+    return true;
+  } else if (num % 2 == 0 || n % 3 == 0) {
+    return false;
+  }
 
+  var i = 5;
+  while (i * i <= num) {
+    if (n % i == 0 || n % (i+2) == 0) {
+      return false;
+    }
+
+    i = i + 6;
+  }
+
+  return true;
 }
 
 //Run and Test function
